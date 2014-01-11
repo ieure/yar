2e3872cbb1d5d261cfd826c32ba9aa0b568f8422
Ian Eure <ian.eure@gmail.com>
2014-01-11 10:28:03 -0800

    Fix test_control

--- yar/tests/test_control.py
+++ yar/tests/test_control.py
@@ -6,15 +6,15 @@
 
 import unittest
 
-from yar.control import Control
+from yar.control import decode_errors
 
 class ErrorDecodeTest(unittest.TestCase):
 
     def test_error_decoding(self):
-        c = Control(None)
 
-        print c.decode_errors(0x80C80081)
-
+        self.assertEqual(
+            decode_errors(0x80C80081),
+            "Error: Programming error Start line not set high Device not blank RAM error RAM end not on 1K boundary")
 
 if __name__ == '__main__':
     unittest.main()
