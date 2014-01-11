46eb94c7b21aba28789c13b6e240cbf543dd593f
Ian Eure <ian.eure@gmail.com>
2014-01-11 10:29:57 -0800

    Fix test_cksum

--- yar/tests/test_cksum.py
+++ yar/tests/test_cksum.py
@@ -13,7 +13,7 @@ class ChecksumTest(unittest.TestCase):
     def test_checksum(self):
         self.assertEqual(checksum([1, 2]), 3)
         self.assertEqual(checksum([256, 256]), 512)
-        self.assertEqual(checksum([65530, 16384]), 16378)
+        self.assertEqual(checksum([0xfffa, 0x4000]), 0x013ffa)
 
 
 if __name__ == '__main__':
