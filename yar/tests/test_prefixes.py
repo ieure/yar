2922a470e9a0a36f90f50e64c9a84d0caf41773d
Ian Eure <ian.eure@gmail.com>
2014-01-12 10:24:31 -0800

    Add asserts in test_prefixes

--- yar/tests/test_prefixes.py
+++ yar/tests/test_prefixes.py
@@ -10,10 +10,15 @@ from yar.devices.prefixes import prefixes
 class PrefixesTest(unittest.TestCase):
 
     def test_get_mfgrs(self):
-        print prefixes.get_mfgrs("sn")
+        self.assertEqual(prefixes.get_mfgrs("sn"), set(['TEX', 'MOT']))
 
     def test_get_prefixes(self):
-        print prefixes.get_mfgrs("TEX")
+        self.assertEqual(
+            prefixes.get_prefixes("TEX"),
+            ('mc', 'ne', 'op', 'rc', 'sg', 'sn', 'tpbpal', 'til',
+             'tip', 'tipal', 'tis', 'tl', 'tlc', 'tle', 'tm',
+             'tms', 'ua', 'uln'))
+
 
 if __name__ == '__main__':
     unittest.main()
